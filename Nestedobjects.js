905ba4dc55ff4ecb8e650faf2e6ed07c70f56623
vmvijay677 <94065738+vmvijay677@users.noreply.github.com>
2021-11-16 21:33:17 +0530

    Create Nestedobjects.js

---
+++ Nestedobjects.js
@@ -0,0 +1,32 @@
+let memory_summary = [{
+	    "maximumcapacity": "2048 MBytes",
+	    "maximum_memory_module_size": "4096 MBytes",
+	    "main": [{
+	        "memory_slots": 4,
+	        "error_correction": "None",
+	        "dram_frequency": "199.0 MHz",
+	        "memory_timings": "3-3-3-8(CL-RCD-RP-RAS)",
+	        "main": []
+	    }]
+	}, {
+	    "device_locator": "slot 1",
+	    "manufacturer": "Hyundai",
+	    "part_number": "HYMD264 646D8J-D43",
+	    "serial_number": "FFFF7240",
+	    "capacity": "512 Mbytes",
+	    "main": [{
+	        "memory_type": "DDR(PC3200)",
+	        "speed": "200 MHz",
+	        "supported_frequencies": "133 MHz",
+	        "memory_timings1": "2-2-2-6-0 at 133 MHz",
+	        "memory_timings2": "2-3-3-7-0 at 166 MHZ",
+	        "memory_timings3": "3-3-3-8-0 at 200 MHz",
+	        "main": [{
+	            "data_width": "64 bits",
+	            "manufacturing_date": 2005,
+	            "epp_spd_support": "No",
+	            "xmp_spd_support": "No",
+	            "main": []
+	            }]
+	    }]
+	}];
